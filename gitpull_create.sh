2af18c31e0772366833fd58d6713631128bf6a5b
sarath raj <sraj@customs.gov.qa>
2020-02-06 12:48:29 +0300

    updated gitpull updated port

--- gitpull_create.sh
+++ gitpull_create.sh
@@ -16,7 +16,7 @@ VOLUME="--mount type=bind,source=$LOG_PATH,destination=/logs "
 UPLOAD_VOLUME="--mount type=bind,source=$UPLOAD_PATH,destination=/uploads "
 #TIMEZONE_FILE="--mount type=bind,source=$LOCALTIME_PATH,destination=/etc/localtime "
 
-docker service create  -d  --network mybridge   --replicas 1 --env-file=./env_qa.txt $VOLUME   $UPLOAD_VOLUME --name treport  -p 4230:4230   172.16.0.253:5000/treport:$TAG
+docker service create  -d  --network mybridge   --replicas 1 --env-file=./env_qa.txt $VOLUME   $UPLOAD_VOLUME --name treport  -p 9085:9085   172.16.0.253:5000/treport:$TAG
 
 
 #docker service update --image   172.16.0.253:5000/treport:$TAG report
